1601fdc0f2cccada8bd4ea65a4932396d0cc58e0
Kaj Lund <kaj.lund@gmail.com>
2026-02-28 22:04:22 +0200

    Adds time tolerance for token verification

--- src/utils/auth.utils.js
+++ src/utils/auth.utils.js
@@ -4,7 +4,7 @@ export function getAuthUtils(cnf, log) {
   return {
     verifyAccessToken: (token) => {
       try {
-        const decoded = jwt.verify(token, cnf.accessTokenSecret);
+        const decoded = jwt.verify(token, cnf.accessTokenSecret, { clockTolerance: 30 });
         return decoded;
       } catch (err) {
         log.error(err);
